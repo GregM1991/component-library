a04926d35a1fb595e3f08f452c3099d3f9555a22
Greg Martin <gregm31@live.com>
2023-07-21 11:47:25 +1000

    removing unecessary types and vars

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -1,29 +1,12 @@
 import { createGlobalStyle, css } from "styled-components";
-import {
-  // DEFAULT_SPACES,
-  // DEFAULT_STEPS,
-  SPACES_PREFIX,
-  STYLE_RESET,
-  TYPE_STEP_PREFIX,
-} from "./constants";
-import { GlobalStyleProps } from "./types";
-import { printCssVars } from "./utils";
+import { STYLE_RESET } from "./constants";
+export interface GlobalStyleProps {
+  withStyleReset?: boolean;
+}
 
 export const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
-  ${({ withStyleReset, steps, spaces }) => css`
+  ${({ withStyleReset }) => css`
     :root {
-      /* Typography steps */
-      ${steps
-        ? printCssVars(steps, TYPE_STEP_PREFIX)
-        : printCssVars(DEFAULT_STEPS, TYPE_STEP_PREFIX)}
-
-      /* Spaces */
-      ${spaces
-        ? printCssVars(spaces, SPACES_PREFIX)
-        : printCssVars(DEFAULT_SPACES, SPACES_PREFIX)}
-    }
-
-    ${withStyleReset && STYLE_RESET}
-  `}
-
+      ${withStyleReset && STYLE_RESET}
+    `}
 `;
